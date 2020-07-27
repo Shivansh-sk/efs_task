3f75cb8ac2c1c1141a5b8feabf1d468ceefbbaee
Shivansh Khandelwal <59771258+Shivansh-sk@users.noreply.github.com>
2020-07-27 17:46:01 +0530

    final

---
+++ index.php
@@ -0,0 +1,11 @@
+Welcome to Shivansh Khandelwal's page
+<pre>
+<html>
+<body>
+<?php
+ print `ifconfig`;
+echo "<img src='https://buckettask1.s3.ap-south-1.amazonaws.com/dog.916.jpg' , width='499', height='392' />";
+?>
+</body>
+</html>
+</pre>
